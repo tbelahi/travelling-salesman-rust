c994a48478d11ba477a8273a82fd0f15469545b5
Thomas Belahi <thomasbelahi@gmail.com>
2016-11-01 13:05:53 +0100

    define travel plan, match travel plan to cities

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,19 @@ fn main() {
     println!("latitude: {}", cities[5].lat);
     println!("la distance entre {} et {} est {} km", 
                 cities[6].name, cities[8].name, cities[6].distance(&cities[8]));
-
+    
+    let plan = load_travel_plan_from_file("./ressources/travel-plan.txt");
+    println!("travel plan: {:?}", plan);
+    let nantes = name_to_city(format!("Nantes"), &cities);
+    println!("{:?}", nantes);
+    let villes: Vec<&City> = plan.into_iter().map(|x| name_to_city(x, &cities)).collect();
+    println!("{:?}", villes);
+    //let cout = zip(villes[0..-1], villes[1..]).fold(0, |&mut acc(x, y| acc +  x.distance(y)).unwrap();
+    let len = villes.len();
+    let mut cout: f64 = 0f64;
+    for i in 0..len-1 {
+        println!("la distance entre {} et {} est {} km", 
+                villes[i].name, villes[i+1].name, villes[i].distance(&villes[i+1]));
+        cout = cout + villes[i].distance(villes[i+1])
+    }
 }
--- src/travel.rs
+++ src/travel.rs
@@ -3,6 +3,10 @@
 use csv;
 use std::f64;
 use std::path::Path;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
 
 pub fn hello_from_travel(dest: &City) {
     // test function to check that travel module can be called from main
@@ -14,7 +18,7 @@ fn degrees_to_rad(angle: f64) -> f64 {
     (angle * f64::consts::PI)/180.0f64
 }
 
-#[derive(Debug,RustcDecodable)]
+#[derive(Debug, RustcDecodable)]
 pub struct City {
     pub name: String,
     pub lat: f64,
@@ -30,24 +34,26 @@ impl City {
         let lat1 = degrees_to_rad(city.lat);
         let lon1 = degrees_to_rad(city.lon);
         // compute the angle variation between the two ciies
-        let dx = lon1.cos() * lat1.cos() - lon0.cos() * lat0.cos();
-        let dy = lon1.cos() * lat1.sin() - lon0.cos() * lat0.sin();
-        let dz = lat1.sin() - lat0.sin();
-        let c = (dx.powi(2) + dy.powi(2) + dz.powi(2)).sqrt();
-        let dsigma = 2.0 * (c/2.0).asin();
+        let dx: f64 = lon1.cos() * lat1.cos() - lon0.cos() * lat0.cos();
+        let dy: f64 = lon1.cos() * lat1.sin() - lon0.cos() * lat0.sin();
+        let dz: f64 = lat1.sin() - lat0.sin();
+        let c : f64= (dx.powf(2.0) + dy.powf(2.0) + dz.powf(2.0)).sqrt();
+        let dsigma: f64 = 2.0 * ((c/2.0f64).asin());
         // Earth's radius
-        const RADIUS: f64 = 6378f64;
+        const RADIUS: f64 = 6400f64;
         // return distance in km
         RADIUS * dsigma  
     }
 }
 
 /// take whatever can be casted to a Path and read the corresponding csv file.
-/// The csv file should be such that
+/// The csv file should be such that:
+///
 /// 1  ville,latitude,longitude
 /// 2  Paris,48.8575,2.3458
 /// 3  Lyon,45.7564,4.8333
 /// 4  ...
+///
 /// The function return a Vec<City>, a list of the cities in the csv file parsed into
 /// the structure City.
 pub fn load_cities_from_file<P: AsRef<Path>>(path: P) -> Vec<City> {
@@ -60,8 +66,42 @@ pub fn load_cities_from_file<P: AsRef<Path>>(path: P) -> Vec<City> {
     cities
 }
 
-// TODO: is it good idea to implement type alias Vec<City> et define function to manipulate it
-// or should I define a new struct Cities {list: Vec<City>, other info...} ?
+/// Loads into a Vec<String> a list of cities to visit for the travelling salesman
+/// The list of cities should be provided as a column in a text file such as:
+/// example.txt
+///
+/// 1  Paris
+/// 2  Toulouse
+/// 3  Montpellier
+/// 4  ...
+///
+pub fn load_travel_plan_from_file(path: &str) -> Vec<String> {
+    // ouvrir le fichier en mode lecture seule
+    let path = Path::new(path);
+    let mut file = match File::open(&path) {
+        // la méthode "descriptive" io::Error retourne une chaine de charactèère
+        // qui décrit l'erreur
+        Err(why) => panic!("couldn't open {:?}: {}", path.display(),
+                           why.description()),
+        Ok(file) => file,
+   };
+
+   let mut s = String::new();
+   file.read_to_string(&mut s).ok().expect("failed to read travel file");
+   let travel_plan: Vec<String> = s.split('\n')
+                                   .map(|x| x.trim().to_string())
+                                   .collect();
+   travel_plan
+}
+
+pub fn name_to_city<'a>(name: String, cities: &'a Vec<City>) -> &'a City {
+    let ix = cities.iter().position(|c| c.name == name).unwrap();
+    &cities[ix]
+}
+
+//pub fn optimize_travel(plan: Vec<String>, cities: Vec<City>) -> (Vec<String>, f64) {
+//
+//}
 
 #[cfg(test)]
 mod tests {
