53f606bcb4bab5491da507b53e0bb1d49b6230ff
Thomas Belahi <thomasbelahi@gmail.com>
2016-11-06 14:51:08 +0100

    nicer main

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,19 @@ mod travel;
 use travel::*;
 
 fn main() {
+    // Welcome
     println!("Salesmen are travelling ...");
 
+    // load resources, IO serve as interface
     let cities: Vec<City> = load_cities_from_file("./ressources/cities.csv");
     let plan = load_travel_plan_from_file("./ressources/travel-plan.txt");
-    let cost = cost_of_travel_plan(&plan, &cities);
-    println!("Total distance of initial travel plan: {} km", cost);
 
+    // compute starting values
+    let cost = cost_of_travel_plan(&plan, &cities);
+    println!("The initial travel plan is: {:?}.", plan);
+    println!("Its cost is: {} km.", cost);
+    
+    // optimize and print resulst
     println!{"Optimizing the travel plan..."}
     let optimized = optimize_travel(&plan, &cities, 2000f64, 4f64, 1000);
     println!("The optimized travel plan is: {:?}.", optimized.0);
