c9017545fc776b928f90b9026aa2097c26891139
Thomas Belahi <thomasbelahi@gmail.com>
2016-11-11 15:06:22 +0100

    corrected code for new behaviour of split

--- src/travel.rs
+++ src/travel.rs
@@ -1,4 +1,4 @@
-//travel.rs
+// travel.rs
 
 use csv;
 use rand;
@@ -18,7 +18,7 @@ pub fn hello_from_travel(dest: &City) {
 
 /// convert degrees to radians
 fn degrees_to_rad(angle: f64) -> f64 {
-    (angle * f64::consts::PI)/180.0f64
+    (angle * f64::consts::PI) / 180.0f64
 }
 
 #[derive(Debug, RustcDecodable)]
@@ -37,11 +37,12 @@ impl City {
         let lat1 = degrees_to_rad(city.lat);
         let lon1 = degrees_to_rad(city.lon);
         // compute the angle variation between the two ciies
-        let dsigma: f64 = (lon0.sin()*lon1.sin()+lon0.cos()*lon1.cos()*(lat1-lat0).cos()).acos();
+        let dsigma: f64 = (lon0.sin() * lon1.sin() + lon0.cos() * lon1.cos() * (lat1 - lat0).cos())
+            .acos();
         // Earth's radius
         const RADIUS: f64 = 6400f64;
         // return distance in km
-        RADIUS * dsigma  
+        RADIUS * dsigma
     }
 }
 
@@ -80,17 +81,17 @@ pub fn load_travel_plan_from_file(path: &str) -> Vec<String> {
     let mut file = match File::open(&path) {
         // la méthode "descriptive" io::Error retourne une chaine de charactèère
         // qui décrit l'erreur
-        Err(why) => panic!("couldn't open {:?}: {}", path.display(),
-                           why.description()),
+        Err(why) => panic!("couldn't open {:?}: {}", path.display(), why.description()),
         Ok(file) => file,
-   };
+    };
 
-   let mut s = String::new();
-   file.read_to_string(&mut s).ok().expect("failed to read travel file");
-   let travel_plan: Vec<String> = s.split('\n')
-                                   .map(|x| x.trim().to_string())
-                                   .collect();
-   travel_plan
+    let mut s = String::new();
+    file.read_to_string(&mut s).ok().expect("failed to read travel file");
+    let travel_plan: Vec<String> = s.split('\n')
+        .map(|x| x.trim().to_string())
+        .filter(|x| !x.is_empty())
+        .collect();
+    travel_plan
 }
 
 pub fn name_to_city<'a>(name: &str, cities: &'a Vec<City>) -> &'a City {
@@ -100,20 +101,20 @@ pub fn name_to_city<'a>(name: &str, cities: &'a Vec<City>) -> &'a City {
 
 pub fn cost_of_travel_plan(plan: &Vec<String>, cities: &Vec<City>) -> f64 {
     let villes: Vec<&City> = plan.into_iter().map(|x| name_to_city(x, &cities)).collect();
-    //println!("{:?}", villes);
-    //let cout = zip(villes[0..-1], villes[1..]).fold(0, |&mut acc(x, y| acc +  x.distance(y)).unwrap();
+    // println!("{:?}", villes);
+    // let cout = zip(villes[0..-1], villes[1..]).fold(0, |&mut acc(x, y| acc +  x.distance(y)).unwrap();
     let len = villes.len();
     let mut cout: f64 = 0f64;
-    for i in 0..len-1 {
-        // println!("la distance entre {} et {} est {} km", 
+    for i in 0..len - 1 {
+        // println!("la distance entre {} et {} est {} km",
         //         villes[i].name, villes[i+1].name, villes[i].distance(&villes[i+1]));
-        cout = cout + villes[i].distance(villes[i+1]);
+        cout = cout + villes[i].distance(villes[i + 1]);
     }
     cout
 }
 
-fn shuffle(plan: &Vec<String>, iteration: &i32) ->  Vec<String> {
-    let between = Range::new(0, plan.len()-1);
+fn shuffle(plan: &Vec<String>, iteration: &i32) -> Vec<String> {
+    let between = Range::new(0, plan.len() - 1);
     let mut rng = rand::thread_rng();
     let pos0 = between.ind_sample(&mut rng);
     let pos1 = between.ind_sample(&mut rng);
@@ -123,14 +124,14 @@ fn shuffle(plan: &Vec<String>, iteration: &i32) ->  Vec<String> {
     //     result.push(city);
     // }
 
-     if iteration % 2 == 0 {
+    if iteration % 2 == 0 {
         // swap pos0 and pos1
         let city0 = plan[pos0].clone();
         result[pos0] = plan[pos1].clone();
         result[pos1] = city0;
     } else {
         // swap pos0 and neighbour
-        // or swap pos1 and neighbour 
+        // or swap pos1 and neighbour
         if pos0 < pos1 {
             let city = plan[pos0].clone();
             result[pos0] = plan[pos0 + 1].clone();
@@ -142,21 +143,30 @@ fn shuffle(plan: &Vec<String>, iteration: &i32) ->  Vec<String> {
         }
     }
     result
-} 
+}
 
-fn accept(new_plan: &Vec<String>, old_plan: &Vec<String>, temperature: &f64, cities: &Vec<City>) -> bool {
+fn accept(new_plan: &Vec<String>,
+          old_plan: &Vec<String>,
+          temperature: &f64,
+          cities: &Vec<City>)
+          -> bool {
     let new_cost = cost_of_travel_plan(&new_plan, &cities);
     let old_cost = cost_of_travel_plan(&old_plan, &cities);
     let mut rng = rand::thread_rng();
     // rng.gen::<f64> uniformely samples [0,1] (or ]0,1[, maybe)
-    if new_cost < old_cost || rng.gen::<f64>() < ((old_cost - new_cost)/temperature).exp() {
+    if new_cost < old_cost || rng.gen::<f64>() < ((old_cost - new_cost) / temperature).exp() {
         true
     } else {
         false
     }
 }
 
-pub fn optimize_travel(plan: &Vec<String>, cities: &Vec<City>, init_temp: f64, cooling_speed: f64, max_iter: i32) -> (Vec<String>, Vec<f64>) {
+pub fn optimize_travel(plan: &Vec<String>,
+                       cities: &Vec<City>,
+                       init_temp: f64,
+                       cooling_speed: f64,
+                       max_iter: i32)
+                       -> (Vec<String>, Vec<f64>) {
     let mut temperature = init_temp;
     let mut couts: Vec<f64> = Vec::new();
     let mut iter: i32 = 0;
@@ -169,10 +179,10 @@ pub fn optimize_travel(plan: &Vec<String>, cities: &Vec<City>, init_temp: f64, c
         if accept(&new, &old, &temperature, &cities) {
             old = new.clone();
         }
-        temperature = init_temp * (-(iter as f64)/(max_iter as f64)*cooling_speed).exp();
+        temperature = init_temp * (-(iter as f64) / (max_iter as f64) * cooling_speed).exp();
         // if iter % 10 == 0 {
         //     println!("Iteration: {}\nTemperature: {}, cost: {}", iter, temperature, couts[(iter - 1) as usize]);
-        // } 
+        // }
     }
     (new, couts)
 }
@@ -184,14 +194,26 @@ mod tests {
 
     #[test]
     fn test_distance() {
-        let paris = City { name: "Paris".to_string(), lon: 2.3476, lat: 48.8543 };
+        let paris = City {
+            name: "Paris".to_string(),
+            lon: 2.3476,
+            lat: 48.8543,
+        };
         assert!(paris.distance(&paris) == 0f64);
     }
 
     #[test]
     fn commutative_test() {
-        let paris = City { name: "Paris".to_string(), lon: 2.3476, lat: 48.8543 };
-        let marseille = City { name: "Marseille".to_string(), lon: 5.3700, lat: 43.2948 };
+        let paris = City {
+            name: "Paris".to_string(),
+            lon: 2.3476,
+            lat: 48.8543,
+        };
+        let marseille = City {
+            name: "Marseille".to_string(),
+            lon: 5.3700,
+            lat: 43.2948,
+        };
         assert!(paris.distance(&marseille) == marseille.distance(&paris));
     }
 }
